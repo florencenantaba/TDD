e14475371f5da2d86f1ebabb60f65e612937cf40
Florence <florencenantabak@gmail.com>
2023-06-25 21:52:45 +0300

    change

--- test/test.js
+++ test/test.js
@@ -2,4 +2,5 @@ const { assert } = require('chai');
 const multiply = require('./multiply.js');
 
 // assert.equal(multiply(1, 1), 1);
-assert.equal(multiply(3, 3), 9);
+// assert.equal(multiply(3, 3), 9);
+assert.equal(multiply(2, 2), 4);
