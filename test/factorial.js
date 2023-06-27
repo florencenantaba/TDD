b38214c3d152bf1a0b033527b1b5e4ade99c9fa6
Florence <florencenantabak@gmail.com>
2023-06-27 10:48:44 +0300

    some changes

---
+++ test/factorial.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+// kalungi florence Edwin//
+let x = 1;
+for (i = 1; i <= 7; i++) {
+  x *= i;
+}
+console.log(x);
--- test/test.js
+++ test/test.js
@@ -3,8 +3,25 @@ const { assert } = require('chai');
 const multiply = require('./multiply');
 
 // eslint-disable-next-line linebreak-style
-assert.equal(multiply(1, 1), 1);
+// assert.equal(multiply(1, 1), 1);
 // assert.equal(multiply(3, 3), 9);
 // assert.equal(multiply(2, 2), 4);
 // assert.equal(multiply(4, 4), 16);
 // assert.equal(multiply(23, 45), 23 * 45);
+describe('return multiple of two numbers',()=>{
+    it('1 * 1===1',()=>{
+        assert.equal(multiply(1,1),1)
+    })
+    it('3 * 3===9',()=>{
+        assert.equal(multiply(3,3),9)
+    })
+    it('2 * 2===4',()=>{
+        assert.equal(multiply(2,2),4)
+    })
+    it('4 * 4===16',()=>{
+        assert.equal(multiply(4,4),16)
+    })
+    it('23 * 45===1035',()=>{
+        assert.equal(multiply(23,45),1035)
+    })
+})
